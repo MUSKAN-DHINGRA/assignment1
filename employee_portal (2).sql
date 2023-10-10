d135817e8134696224c799dfaae74844bc2e62c2
muskan dhingra <146599472+MUSKAN-DHINGRA@users.noreply.github.com>
2023-10-10 12:33:59 -0400

    Add files via upload

---
+++ employee_portal (2).sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 10, 2023 at 04:14 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `employee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_portal`
+--
+
+DROP TABLE IF EXISTS `employee_portal`;
+CREATE TABLE IF NOT EXISTS `employee_portal` (
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `age` int NOT NULL,
+  `email` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `hoursworked` int NOT NULL,
+  `department` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `employee_portal`
+--
+
+INSERT INTO `employee_portal` (`fname`, `lname`, `age`, `email`, `hoursworked`, `department`) VALUES
+('Muskan', 'Dhingra', 20, 'muskanhub01@gmail.com', 20, 'Management'),
+('Muskan', 'Dhingra', 20, 'muskanhub01@gmail.com', 20, 'Management'),
+('Muskan', 'Dhingra', 0, 'muskanhub01@gmail.com', 15, 'Management');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
