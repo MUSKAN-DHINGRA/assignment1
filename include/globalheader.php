d69896c3b1ee32996edc81b9bc324ad5f19766f7
muskan dhingra <146599472+MUSKAN-DHINGRA@users.noreply.github.com>
2023-11-15 21:45:03 -0500

    Intial commit - commiting all files to the repository

--- include/globalheader.php
+++ include/globalheader.php
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Home</a></li>
-            <li class="nav-item"><a class="nav-link" href="view.php">View</a></li>
+            <li class="nav-item"><a class="nav-link" href="view_employee.php">View</a></li>
           </ul>
         </div>
       </div>
